4c64806b14827c55d63ad0a7151b738aabfe9f8d
Vinicius Freire <82891202+Vinicius-FreireCod@users.noreply.github.com>
2022-10-06 00:43:10 -0300

    added gitignore #8 (#11)
    * added gitignore #8
    
    * Set up project initial organization and metadata
    
    * fixed newline as requested
    
    Closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward point System for dunder Mifflin",
+    author="Vinicius Freire",
+    packages=find_packages(),
+)
